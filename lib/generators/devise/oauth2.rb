c254367b27111a02f1de17dbf690b3d772413f50
Guido Tarsia <guidotarsia@gmail.com>
2018-07-31 19:44:18 -0300

    Remove comments and username from devise

--- lib/generators/devise/oauth2.rb
+++ lib/generators/devise/oauth2.rb
@@ -16,44 +16,44 @@ module Wurk
         end
       end
 
-      # def set_routes_devise_for
-      #   gsub_file 'config/routes.rb', /devise_for :users/ do
-      #     "devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }"
-      #   end
-      # end
+      def set_routes_devise_for
+        gsub_file 'config/routes.rb', /devise_for :users/ do
+          "devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }"
+        end
+      end
 
-      # def set_user_omniauthable
-      #   gsub_file 'app/models/user.rb', /devise :/ do
-      #     "devise :omniauthable, :"
-      #   end
-      # end
+      def set_user_omniauthable
+        gsub_file 'app/models/user.rb', /devise :/ do
+          "devise :omniauthable, :"
+        end
+      end
 
-      # def omniauth_callbacks_controller
-      #   copy_file 'omniauth_callbacks_controller.rb', 'app/controllers/users/omniauth_callbacks_controller.rb'
-      # end
+      def omniauth_callbacks_controller
+        copy_file 'omniauth_callbacks_controller.rb', 'app/controllers/users/omniauth_callbacks_controller.rb'
+      end
 
-      # def user_omniauth
-      #   inject_into_class 'app/models/user.rb', User do
-      #     <<-HEREDOC
-  # def self.from_omniauth(access_token)
-    # data = access_token.info.to_h.symbolize_keys
-    # data[:image_url] = extract_google_image_url access_token
+      def user_omniauth
+        inject_into_class 'app/models/user.rb', User do
+          <<-HEREDOC
+  def self.from_omniauth(access_token)
+    data = access_token.info.to_h.symbolize_keys
+    data[:image_url] = extract_google_image_url access_token
 
-    # updateable_fields = %i[name image_url]
-    # all_fields = (updateable_fields + [:email]).uniq
+    updateable_fields = %i[name image_url]
+    all_fields = (updateable_fields + [:email]).uniq
 
-    # user = User.where(data.slice(:email)).first
+    user = User.where(data.slice(:email)).first
 
-    # if user
-    # user.update data.slice(*updateable_fields)
-    # else
-    # user = User.create data.slice(*all_fields)
-    # end
-    # user
-  # end
-      #       HEREDOC
-      #   end
-      # end
+    if user
+    user.update data.slice(*updateable_fields)
+    else
+    user = User.create data.slice(*all_fields)
+    end
+    user
+  end
+            HEREDOC
+        end
+      end
     end
   end
 end
